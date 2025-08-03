329701babcd1d4f6f28950819a92bf07a078e1db
VundaRoy <roychang@live.com.au>
2025-08-03 17:56:16 +1000

    Few react editing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import shangrilaImage from './Image/shangrila.jpg';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +12,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+function MyComponent() {
+  return (
+    <div>
+      <h1>My Holiday</h1>
+      {/* 2. Use the imported variable in the src attribute */}
+      <img src={shangrilaImage} alt="Holiday" />
+    </div>
+  );
+}
+//export default MyComponent;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
