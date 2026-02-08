17bff1fc4796e8643cb5fd40859919212824b63f
VundaRoy <roychang@live.com.au>
2026-02-08 15:59:58 +1100

    comment index.js to render the jsx command

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import shangrilaImage from './Image/shangrila.jpg';
 import './jsxProcs/proc.jsx';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
    
   </React.StrictMode>
-);
+); */
 function MyComponent() {
   return (
     <div>
